76093e87f0b7eeba2da397663be1439a0aaa5e93
codemasternode <marcinwarzybok@outlook.com>
2019-04-13 20:29:17 +0200

    v1

--- src/models/users.js
+++ src/models/users.js
@@ -1,27 +1,35 @@
 const user = (sequelize, DataTypes) => {
-    const User = sequelize.define('user', {
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false
+    const User = sequelize.define('user',
+        {
+            first_name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            last_name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            isAdmin: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            }
         },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        isAdmin: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-        }
-    });
+        {
+            instanceMethods: {
+                comparePassword: function (candidatePassword, cb) {
+                    
+                }
+            }
+        });
 
     User.associate = models => {
         User.belongsToMany(models.Classes, {
